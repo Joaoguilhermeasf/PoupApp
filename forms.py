53b9427337799c4426facc9f3a8ef6f12bf6bc49
Lucas Fernandes <lucas.fernandes@g-iatech.com>
2024-06-04 15:08:18 -0300

    Telas cadastro e login

--- forms.py
+++ forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, SubmitField, PasswordField, IntegerField, DateField, BooleanField, RadioField, SelectField, TextAreaField, SelectField
 
-class UserForms(FlaskForm):
+class UserForm(FlaskForm):
     login = StringField('Login: ',[validators.DataRequired(),validators.Length(min=1,max=20)])
     password = PasswordField('Senha: ',[validators.DataRequired()])
     entrar = SubmitField('Entrar')
+    cadastrar = SubmitField('Cadastrar')
+
+
+class RegForm(FlaskForm):
+    login = StringField('Login: ',[validators.DataRequired(),validators.Length(min=1,max=20)])
+    password = PasswordField('Senha: ',[validators.DataRequired()])
+    repeat_password = PasswordField('Repita a senha: ',[validators.DataRequired()])
     cadastrar = SubmitField('Cadastrar')
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
-from forms import UserForms
+from flask import Flask, render_template, redirect, url_for, request
+from forms import UserForm, RegForm
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
-    form = UserForms()
+    form = UserForm()
     return render_template('index.html', form=form)
 
+
+@app.route('/cadastrar/', methods=['GET', 'POST'])
+def cadastrar():
+    form = RegForm()
+    if request.method == 'POST':
+        return 'CADASTRADO'
+
+    return render_template('cadastrar.html', form=form)
 if __name__ == '__main__':
     app.run(debug=True)
